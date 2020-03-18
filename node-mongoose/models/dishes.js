746ee57908839a09c8c8dea8976f5527aaadc51e
Haowen Jiang <534878350@qq.com>
2020-03-18 20:25:26 +0800

    。。

---
+++ node-mongoose/models/dishes.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema ({
+    rating : {
+        type : Number,
+        min : 1,
+        max : 5,
+        required : true
+    },
+    comment : {
+        type : String,
+        required : true
+    },
+    author : {
+        type : String,
+        required : true
+    }
+},{
+    timestamps : true
+}
+)
+
+const dishSchema = new Schema({
+    name : {
+        type : String,
+        require : true,
+        unique : true,
+    },
+    description : {
+        type : String,
+        require : true
+    },
+    comments : [commentSchema]
+}, {
+    timestamps : true
+}
+);
+
+var Dishes = mongoose.model('Dish' ,dishSchema);
+
+module.exports = Dishes;
