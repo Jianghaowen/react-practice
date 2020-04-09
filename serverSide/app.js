af08114503e79940ba8353bab6d190d4fcba9e34
Haowen Jiang <534878350@qq.com>
2020-04-09 23:16:51 +0800

    token

--- serverSide/app.js
+++ serverSide/app.js
@@ -1,20 +1,27 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+// var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+// var authenticate = require('./authenticate');
+var config = require('./config');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter =  require('./routes/dishRouter')
 var promoRouter = require('./routes/promoRouter')
 var leaderRouter =  require('./routes/leaderRouter')
+var uploadRouter =  require('./routes/uploadRouter')
+var favoriteRouter = require('./routes/favoriteRouter')
+var commentRouter = require('./routes/commentsRouter');
 
 const mongoose = require('mongoose');
 
-const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -25,6 +32,15 @@ connect.then((db) => {
 
 var app = express();
 
+app.all('*' , (req ,res , next) => {
+  if(req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307,'https://' + req.hostname+ ':' +  app.get('secPort') + req.url);
+  }
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -32,41 +48,47 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser('12345-67890-09876-54321'));
 
-function auth (req, res, next) {
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-  }
+// app.use(session({
+//   name: 'session-id',
+//   secret: '12345-67890-09876-54321',
+//   saveUninitialized: false,
+//   resave: false,
+//   store: new FileStore()
+// }));
 
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == 'admin' && pass == 'password') {
-      next(); // authorized
-  } else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
-      err.status = 401;
-      next(err);
-  }
-}
-
-app.use(auth);
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// function auth (req, res, next) {
+//   console.log(req.user);
+
+//   if (!req.user) {
+//     var err = new Error('You are not authenticated!');
+//     err.status = 403;
+//     next(err);
+//   }
+//   else {
+//         next();
+//   }
+// }
+
+// app.use(auth);
+
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
+app.use('/favorites' , favoriteRouter);
+app.use('/comments',commentRouter);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ serverSide/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey' : '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId : '533263330950733' ,
+        clientSecret : '2fc24f2369080c2b917e99e6891c1623'
+    }
+}
